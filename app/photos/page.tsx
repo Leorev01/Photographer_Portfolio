734460aeb3f86d7fd5621df4aa400e6fae2bc811
Leorev01 <revrennaleo@gmail.com>
2024-11-17 18:55:37 +0000

    Adds some photos to photos page

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -2,7 +2,16 @@ import ImageGrid from "@/components/ImageGrid"
 
 const page = () => {
   return (
-    <ImageGrid/>
+    <div className="bg-black">
+      <div className="container mx-auto pt-14">
+        <h1 className="text-4xl text-white text-center py-10">
+          Photos
+          </h1>
+        <ImageGrid/>
+      </div>
+      
+    </div>
+    
   )
 }
 
--- components/ImageGrid.tsx
+++ components/ImageGrid.tsx
@@ -4,56 +4,88 @@ import curlyHairImage from '@/public/images/curly-hair-girl.jpg'
 import jamaicaHouseImage from '@/public/images/jamaica-house.jpg'
 import jamaicaStreetImage from '@/public/images/jamaica-street.jpg'
 import flowerImage from '@/public/images/white-flower.jpg'
+import loneTree from '@/public/images/lone-tree.jpg'
+import girlSelfie from '@/public/images/girl-selfie.jpg'
+import londonCity from '@/public/images/london-city.jpg'
 
 const ImageGrid = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-5 lg:px-20">
-      {/* First image spans two columns and has a taller height on larger screens */}
-      <div className="relative col-span-2 h-72 sm:h-96 lg:h-[400px]">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 px-5 lg:px-20">
+      
+      {/* First image spans two columns */}
+      <div className="relative col-span-2 lg:col-span-1 h-72 sm:h-96 lg:h-[500px] xl:h-[600px] group">
         <Image 
           src={jamaicaHouseImage} 
           alt="Jamaica house" 
-          className="object-cover w-full h-full rounded-lg shadow-lg transition-all duration-300 hover:scale-105" 
+          className="object-cover w-full h-full rounded-lg shadow-2xl group-hover:scale-105 transition-all duration-300"
         />
       </div>
       
-      {/* Second image */}
-      <div className="relative h-72 sm:h-96 lg:h-[400px]">
+      {/* Second image spans one column */}
+      <div className="relative h-72 sm:h-96 lg:h-[400px] xl:h-[500px] group">
         <Image 
           src={jamaicaStreetImage} 
-          alt="jamaica street" 
-          className="object-cover w-full h-full rounded-lg shadow-lg transition-all duration-300 hover:scale-105" 
+          alt="Jamaica street" 
+          className="object-cover w-full h-full rounded-lg shadow-2xl group-hover:scale-105 transition-all duration-300"
         />
       </div>
 
-      {/* Third image */}
-      <div className="relative h-72 sm:h-96 lg:h-80">
+      {/* Third image spans two columns */}
+      <div className="relative col-span-2 h-72 sm:h-96 lg:h-[500px] xl:h-[600px] group">
         <Image 
           src={curlyHairImage} 
           alt="Curly hair girl" 
-          className="object-cover w-full h-full rounded-lg shadow-lg transition-all duration-300 hover:scale-105" 
+          className="object-cover w-full h-full rounded-lg shadow-2xl group-hover:scale-105 transition-all duration-300"
         />
       </div>
 
-      {/* Fourth image */}
-      <div className="relative h-72 sm:h-96 lg:h-80">
+      {/* Fourth image spans one column */}
+      <div className="relative h-72 sm:h-96 lg:h-[400px] xl:h-[500px] group">
         <Image 
           src={boatImage} 
           alt="Boat in night" 
-          className="object-cover w-full h-full rounded-lg shadow-lg transition-all duration-300 hover:scale-105" 
+          className="object-cover w-full h-full rounded-lg shadow-2xl group-hover:scale-105 transition-all duration-300"
         />
       </div>
 
-      {/* Fifth image */}
-      <div className="relative h-72 sm:h-96 lg:h-80">
+      {/* Fifth image spans three columns */}
+      <div className="relative col-span-3 lg:col-span-2 h-72 sm:h-96 lg:h-[500px] xl:h-[600px] group">
         <Image 
           src={flowerImage} 
           alt="White flower" 
-          className="object-cover w-full h-full rounded-lg shadow-lg transition-all duration-300 hover:scale-105" 
+          className="object-cover w-full h-full rounded-lg shadow-2xl group-hover:scale-105 transition-all duration-300"
         />
       </div>
+
+      {/* New image 1 */}
+      <div className="relative h-72 sm:h-96 lg:h-[400px] xl:h-[500px] group">
+        <Image 
+          src={loneTree} 
+          alt="Lone tree" 
+          className="object-cover w-full h-full rounded-lg shadow-2xl group-hover:scale-105 transition-all duration-300"
+        />
+      </div>
+
+      {/* New image 2 spans two columns */}
+      <div className="relative col-span-2 lg:col-span-1 h-72 sm:h-96 lg:h-[500px] xl:h-[600px] group">
+        <Image 
+          src={girlSelfie} 
+          alt="Girl selfie" 
+          className="object-cover w-full h-full rounded-lg shadow-2xl group-hover:scale-105 transition-all duration-300"
+        />
+      </div>
+
+      {/* New image 3 spans one column */}
+      <div className="relative h-72 sm:h-96 lg:h-[400px] xl:h-[500px] group">
+        <Image 
+          src={londonCity} 
+          alt="London city" 
+          className="object-cover w-full h-full rounded-lg shadow-2xl group-hover:scale-105 transition-all duration-300"
+        />
+      </div>
+
     </div>
   )
 }
 
-export default ImageGrid
+export default ImageGrid;
