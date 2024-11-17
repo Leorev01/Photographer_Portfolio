b05ba0f119c18d4a99ef4d51318ff68685d4503e
Leorev01 <revrennaleo@gmail.com>
2024-11-17 19:18:57 +0000

    Adds About page

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div className='bg-black pt-14'>
+        <h1 className="text-4xl text-white text-center py-10">
+            About Me
+        </h1>
+    </div>
+  )
+}
+
+export default AboutPage
