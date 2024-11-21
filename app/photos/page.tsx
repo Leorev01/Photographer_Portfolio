1dd85904f22f96d73f18a7101a03fadc727c68bd
Leorev01 <revrennaleo@gmail.com>
2024-11-21 21:09:09 +0000

    Adds more photos

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -4,9 +4,6 @@ const page = () => {
   return (
     <div className="bg-black">
       <div className="container mx-auto pt-14">
-        <h1 className="text-4xl text-white text-center py-10">
-          Photos
-          </h1>
         <ImageGrid/>
       </div>
       
